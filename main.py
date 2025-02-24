2a0b9a6555d81ef6d25fa753e49c10d2b5d47aae
TheDiamondArm <86077088+TheDiamondArm@users.noreply.github.com>
2025-02-24 10:16:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "https://yandex.ru",    
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,    
+    allow_credentials=True,     
+    allow_methods=["*"],     
+    allow_headers=["*"],       
+)
 
 @app.get("/", response_class=PlainTextResponse)
 async def root():
